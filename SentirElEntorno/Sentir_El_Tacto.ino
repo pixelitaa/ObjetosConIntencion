b44bac823fd5de65d56073b485a0e37c14f6613d
Paulita Landeros <paula.landeros@mail.udp.cl>
2026-03-27 01:13:39 -0300

    Create Sentir_El_Tacto.ino

---
+++ SentirElEntorno/Sentir_El_Tacto.ino
@@ -0,0 +1,18 @@
+// Ejercicio 2.2: El Sensor de Presión
+const int pinPresion = A1;
+int umbralFuerza = 100; // Sensibilidad del toque, valor entre 0 y 1024 la sensibilidad de este es muy variable dependiendo del sensor.
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int fuerza = analogRead(pinPresion);
+  
+  if (fuerza > umbralFuerza) {
+    Serial.println("ESTADO: AAAAAAAA ME DUELEEEEEE");
+  } else {
+    Serial.println("ESTADO: estoy tan solo....");
+  }
+  delay(200);
+}
